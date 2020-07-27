fe6a74b9013fa009d2e3a706d470b2c22ae85cfd
polmonroig <polmonroig@yahoo.es>
2020-07-27 21:42:46 +0200

    Add utility functions to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,29 @@
 import os
 
+# basic utility functions 
+def listdir_complete(path):
+    """
+    Equivalent to os.listdir, appends 
+    the complete path to each file 
+    """
+    return [os.path.join(path, f) for f in os.listdir(path)]
+
+def count_unique(data):
+    """
+    Counts the quantity of unique values in a list 
+    of values 
+    """
+    count = {}
+    for d in data:
+        d = d.split('-')[0]
+        if d in count:
+            count[d] += 1 
+        else:
+            count[d] = 1 
+    values = [count[k] for k in sorted(count.keys())]
+    total = np.arange(len(count))
+    return total, values 
+
 # general path variables
 # each path variable is defined by *_dir 
 data_dir = '../data'
@@ -12,3 +36,5 @@ train_file = os.path.join(data_dir, 'train.csv')
 test_file = os.path.join(data_dir, 'test.csv')
 example_test_audio_metadata_file = os.path.join(data_dir, 'example_test_audio_metadata.csv')
 example_test_audio_summary_file = os.path.join(data_dir, 'example_test_audio_summary.csv')
+train_audio_files = listdir_complete(train_audio_dir)
+example_audio_files = listdir_complete(example_test_audio_dir)
