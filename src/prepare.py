1d445d105dbdaf6de00588c0e7132d2d39595b46
polmonroig <polmonroig@yahoo.es>
2020-08-01 14:07:07 +0200

    Add arg parser to prepare.py

--- src/prepare.py
+++ src/prepare.py
@@ -1,10 +1,35 @@
 from utils import filesystem
-from utils import sound 
+from utils import sound
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Prepare data from training')
+    parser.add_argument('--chunk_size', dest='chunk_size',
+        help='Audio chunk size in seconds', type=integer)
+    parser.add_argument('--min_chunk_size', dest='min_chunk_size', type='integer',
+        help='Defines the minimum size that the chunk must have to padd it')
+
+    return parser
 
 def main():
-    print('Initializing dta preparation...')
+    """
+    Prepares the data for training, this includes:
+        - Data augmentation
+        - Data filtering
+        - Waveform chunk subdivision
+    """
+    print('Initializing data preparation...')
     audio_directories = filesystem.train_audio_files
-    print(audio_directories)
+    sample_class = audio_directories[0]
+    print('Selecting class', sample_class)
+    audio_files = filesystem.listdir_complete(sample_class)
+    print('Total audio files', len(audio_files))
+    print('Loading file:', audio_files[0])
+    sample_audio = sound.load_audio(audio_files[0])
+    print('Waveform length:', len(sample_audio[0][0]))
+    print('Waveform sample rate:', sample_audio[1])
+    sample_audio = sound.divide_into_chunks(sample_audio, )
 
 if __name__ == '__main__':
     main()
--- src/utils/sound.py
+++ src/utils/sound.py
@@ -1,7 +1,22 @@
-import librosa
+import torchaudio
+import torch
 
 def load_audio(path, mono=True):
     """
-    Return the audio data in mono form of a file  
+    Return the audio data in mono form of a file and the sample
+    rate
     """
-    return librosa.load(path=path, mono=mono)[0]
+    return torchaudio.load(path)
+
+
+def divide_into_chunks(audio, chunk_size, min_size):
+    """
+    Given an audiio waveform, it divides it into equal size
+    chunks. The final chunk may be discarded or padded.
+
+    audio: the waveform to subdivide
+    chunk_size: the size of the chunk in seconds
+    min_size: defines the minimum size that the chunk must have to padd it
+
+    """
+    return torch.tensor(audio)
