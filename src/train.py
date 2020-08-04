5219427469dce6f85091ca697ca0c1c691d3098b
polmonroig <polmonroig@yahoo.es>
2020-08-04 17:57:46 +0200

    Add automatic device selection

--- src/train.py
+++ src/train.py
@@ -1,7 +1,17 @@
 from model import FeatureExtractor 
+import torch.optim as optim 
+import torch.nn as nn 
+import torch 
+import wandb
 
 def main():
     print('Initializing model training...')
+    device = torch.device('cpu')
+    if torch.cuda.is_available(): 
+        print('Running on gpu')
+        device = torch.device('cuda')
+    else:
+        print('Running on cpu')
 
 
 if __name__ == '__main__':
