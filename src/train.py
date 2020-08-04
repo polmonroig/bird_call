5b48bcc9d26c91dee9e3da7879105fc4939185d1
polmonroig <polmonroig@yahoo.es>
2020-08-04 18:05:01 +0200

    Add argumetn parser to train.py

--- src/train.py
+++ src/train.py
@@ -3,9 +3,26 @@ import torch.optim as optim
 import torch.nn as nn 
 import torch 
 import wandb
+import argparse 
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Train the neural network')
+
+    return parser 
+
+
+def train_autoencoder(device):
+    model = FeatureExtractor() 
+    model.to(device)
+    print(model) 
+
+def train_classifier(device):
+    raise NotImplementedError()
 
 def main():
     print('Initializing model training...')
+    parser = get_parser() 
+    args = parser.parse_args() 
     device = torch.device('cpu')
     if torch.cuda.is_available(): 
         print('Running on gpu')
@@ -13,6 +30,8 @@ def main():
     else:
         print('Running on cpu')
 
+    train_autoencoder(device)
+    #train_classifier(device)
 
 if __name__ == '__main__':
     main()
