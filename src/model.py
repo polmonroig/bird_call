e09b0b895ca402271a4e90d06a4264f25f04b318
polmonroig <polmonroig@yahoo.es>
2020-08-04 15:34:41 +0200

    Add dummy network to feature extractor

--- src/model.py
+++ src/model.py
@@ -6,12 +6,24 @@ class FeatureExtractor(nn.Module):
 
     def __init__(self):
         super().__init__()
-        
+        self.encoder = nn.Sequential(
+                        nn.Conv1d(in_channels=1, out_channels=16, 
+                            kernel_size=3, stride=0, padding=0), 
+                        nn.Conv1d(in_channels=16, out_channels=32), 
+                        nn.Conv1d(in_channels=32, out_channels=64)
+                        )
+        self.decoder = nn.Sequential(
+                        nn.Conv1d(in_channels=64, out_channels=32), 
+                        nn.Conv1d(in_channels=32, out_channels=16), 
+                        nn.Conv1d(in_channels=16, out_channels=1)
+                        )
+
+
     def encode(self, x):
-        return x 
+        return self.encoder(x)
 
     def decode(self, x):
-        return x 
+        return self.decoder(x)
 
     def forward(self, x):
         return self.decode(self.encode(x))
