Fix first softmax was not working

src/model.py
@@ -96,7 +96,7 @@ class Classifier(nn.Module):
             nn.AdaptiveMaxPool1d(1000),
             nn.ReLU(inplace=True),
             nn.Linear(1000, 264),
-            nn.Softmax(dim=1),
+            nn.Softmax(),
         ])
 
 