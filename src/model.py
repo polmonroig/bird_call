31c6e8ab7d2c86fbcea9e3982308263b96f781a7
polmonroig <polmonroig@yahoo.es>
2020-08-17 11:40:08 +0200

    Change back to normal metrics

--- src/model.py
+++ src/model.py
@@ -142,16 +142,14 @@ def train_step_classification(model, data_loader, optimizer, loss_criterion, ver
             out = (out > prediction_threshold)
             one_hot_labels = to_cpu(one_hot_labels)
             out = to_cpu(out)
-            confusion_matrix = multilabel_confusion_matrix(one_hot_labels, out)
-            cv2.imwrite(os.path.join(wandb.run.dir, 'confusion_matrix_'+ i +'_.png'), confusion_matrix)
 
-            #a = accuracy_score(one_hot_labels, out)
-            #f1 = f1_score(one_hot_labels, out, average='micro', zero_division=0)
-            #print('Train accuracy:', a)
-            #print('F1 score:', f1)
-            #print('Train Precision score: ', precision_score(one_hot_labels, out, average='micro', zero_division=1))
-            #print('Train Recall score: ', recall_score(one_hot_labels, out, average='micro', zero_division=1))
-            #wandb.log({'Train Loss' : loss.item(), 'Train accuracy': a, 'F1  score' : f1})
+            a = accuracy_score(one_hot_labels, out)
+            f1 = f1_score(one_hot_labels, out, average='micro', zero_division=0)
+            print('Train accuracy:', a)
+            print('F1 score:', f1)
+            print('Train Precision score: ', precision_score(one_hot_labels, out, average='micro', zero_division=1))
+            print('Train Recall score: ', recall_score(one_hot_labels, out, average='micro', zero_division=1))
+            wandb.log({'Train Loss' : loss.item(), 'Train accuracy': a, 'F1  score' : f1})
 
 
 
